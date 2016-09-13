0eff1a79d72f5fb797f43de71a0de3e0a2e5a7da
floreacosta <florem.10@hotmail.com>
2016-09-12 21:28:47 -0300

    Linkeo del header

--- include/header.php
+++ include/header.php
@@ -1,6 +1,6 @@
 	<header>
 		<div class="logo">
-			<a href="#"><img src="img/logo.png" title="Clínica Los Cedros de Tapiales S.A."/></a>
+			<a href="index.php" title="Los Cedros de Tapiales S.A."><img alt="Los Cedros de Tapiales" src="img/logo.png"/></a>
 		</div>
 		
 		<nav class="menu">
@@ -13,10 +13,10 @@
 			</div>
 			<div class="menu-element">
 				<ul id="element">
-					<li><a href="#" id="instalaciones" title="Instalaciones">Instalaciones</a></li>
-					<li><a href="#" id="especialidades" title="Especialidades">Especialidades</a></li>
-					<li><a href="#" id="coberturas" title="Coberturas Médicas">Coberturas</a></li>
-					<li><a href="#" id="medicina-preventiva" title="Medicina Preventiva">Medicina Preventiva</a></li>
+					<li><a href="instalaciones.php" id="instalaciones" title="Instalaciones">Instalaciones</a></li>
+					<li><a href="especialidades.php" id="especialidades" title="Especialidades">Especialidades</a></li>
+					<li><a href="coberturas-medicas.php" id="coberturas" title="Coberturas Médicas">Coberturas</a></li>
+					<li><a href="medicina-preventiva.php" id="medicina-preventiva" title="Medicina Preventiva">Medicina Preventiva</a></li>
 				</ul>
 			</div>
 		</nav>
