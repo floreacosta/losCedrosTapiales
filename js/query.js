bb4ad66470871fe4cbd338daa0ba32f3727e3971
floreacosta <florem.10@hotmail.com>
2016-09-12 22:10:53 -0300

    Add class con Jquery

--- js/query.js
+++ js/query.js
@@ -1,9 +1,35 @@
-$(window).resize(function(){
-	$widhtWindows = $( window ).width();
+$(document).ready(function() {
+	var URLactual = window.location.pathname;
 
-	if ($widhtWindows >= 1000) {
-		$( "#menu" ).removeClass( "menu-element-mobile" ).addClass( "menu-element-desktop" );
-	} else {
-		$( "#menu" ).removeClass( "menu-element-desktop" ).addClass( "menu-element-mobile" );		
+	switch(URLactual) {
+		case '/losCedrosTapiales/institucional.php':
+			$( "#instalaciones" ).addClass("active");
+			$( "#medicina-preventiva" ).remove("active");
+			$( "#coberturas" ).remove("active");
+			$( "#especialidades" ).remove("active");
+		break;
+		case '/losCedrosTapiales/medicina-preventiva.php':
+			$( "#instalaciones" ).remove("active");
+			$( "#medicina-preventiva" ).addClass("active");
+			$( "#coberturas" ).remove("active");
+			$( "#especialidades" ).remove("active");
+		break;
+		case '/losCedrosTapiales/coberturas-medicas.php':
+			$( "#instalaciones" ).remove("active");
+			$( "#medicina-preventiva" ).remove("active");
+			$( "#coberturas" ).addClass("active");
+			$( "#especialidades" ).remove("active");
+		break;
+		case '/losCedrosTapiales/especialidades.php':
+			$( "#instalaciones" ).remove("active");
+			$( "#medicina-preventiva" ).remove("active");
+			$( "#coberturas" ).remove("active");
+			$( "#especialidades" ).addClass("active");
+		default:
+			$( "#instalaciones" ).remove("active");
+			$( "#medicina-preventiva" ).remove("active");
+			$( "#coberturas" ).remove("active");
+			$( "#especialidades" ).remove("active");
+		break;
 	}
 });
